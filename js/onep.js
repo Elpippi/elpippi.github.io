a7a2ec3890635eb3afbc2366b90329c07329aced
Elpippi <ludper.lp@gmail.com>
2021-03-10 23:19:01 +0100

    singlep

--- js/onep.js
+++ js/onep.js
@@ -1,91 +1,170 @@
-const statusDisplay = document.querySelector(".game--status");
+let origBoard;
+const HUMAN_PLAYER = "O";
+const AI_PLAYER = "X";
 
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
-
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
-statusDisplay.innerHTML = currentPlayerTurn();
-
-const winningConditions = [
+const winCombos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
+
   [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
+
   [0, 4, 8],
   [2, 4, 6],
 ];
 
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-  gameState[clickedCellIndex] = currentPlayer;
-  clickedCell.innerHTML = currentPlayer;
+const cells = document.getElementsByClassName("cell");
+onStartGame();
+
+function onStartGame() {
+  // console.log('Starting Game');
+  document.querySelector(".end-game").style.display = "none";
+  origBoard = Array.from(Array(9).keys());
+  // console.table(origBoard);
+  // console.log(cells);
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", onTurnClick, false);
+  }
 }
 
-function handlePlayerChange() {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-  statusDisplay.innerHTML = currentPlayerTurn();
-}
-
-function handleResultValidation() {
-  let roundWon = false;
-  for (let i = 0; i <= 7; i++) {
-    const winCondition = winningConditions[i];
-    let a = gameState[winCondition[0]];
-    let b = gameState[winCondition[1]];
-    let c = gameState[winCondition[2]];
-    if (a === "" || b === "" || c === "") {
-      continue;
+function onTurnClick(e) {
+  // console.log(e.target.id);
+  const { id: squareId } = e.target;
+  if (typeof origBoard[squareId] === "number") {
+    onTurn(squareId, HUMAN_PLAYER);
+    if (!onCheckGameTie()) {
+      onTurn(botPicksSpot(), AI_PLAYER);
     }
-    if (a === b && b === c) {
-      roundWon = true;
+  } else {
+    const message = "That spot is already taken, click somewhere else";
+    alert(message);
+  }
+}
+
+function onTurn(squareId, player) {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  let isGameWon = onCheckWin(origBoard, player);
+  // console.log(isGameWon)
+  if (isGameWon) {
+    onGameOver(isGameWon);
+  }
+}
+
+function onCheckWin(board, player) {
+  let plays = board.reduce((a, e, i) => {
+    return e === player ? a.concat(i) : a;
+  }, []);
+  let gameWon = false;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWon = {
+        index: index,
+        player: player,
+      };
       break;
     }
   }
-
-  if (roundWon) {
-    statusDisplay.innerHTML = winningMessage();
-    gameActive = false;
-    return;
-  }
-
-  let roundDraw = !gameState.includes("");
-  if (roundDraw) {
-    statusDisplay.innerHTML = drawMessage();
-    gameActive = false;
-    return;
-  }
-
-  handlePlayerChange();
+  return gameWon;
 }
 
-function handleCellClick(clickedCellEvent) {
-  const clickedCell = clickedCellEvent.target;
-  const clickedCellIndex = parseInt(
-    clickedCell.getAttribute("data-cell-index")
-  );
-
-  if (gameState[clickedCellIndex] !== "" || !gameActive) {
-    return;
+function onGameOver({ index, player }) {
+  for (let i of winCombos[index]) {
+    const color = player === HUMAN_PLAYER ? "#2196f3" : "#f44336";
+    document.getElementById(i).style.backgroundColor = color;
+  }
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", onTurnClick, false);
   }
 
-  handleCellPlayed(clickedCell, clickedCellIndex);
-  handleResultValidation();
+  const result = player === HUMAN_PLAYER ? "You Win" : "You Lose";
+  onDeclareWinner(result);
 }
 
-function handleRestartGame() {
-  gameActive = true;
-  currentPlayer = "X";
-  gameState = ["", "", "", "", "", "", "", "", ""];
-  statusDisplay.innerHTML = currentPlayerTurn();
-  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+function onDeclareWinner(who) {
+  // console.log('Result: ', who);
+  document.querySelector(".end-game").style.display = "block";
+  document.querySelector(".end-game .text").innerText = `Result: ${who}`;
 }
 
-document
-  .querySelectorAll(".cell")
-  .forEach((cell) => cell.addEventListener("click", handleCellClick));
-document.querySelector(".button1").addEventListener("click", handleRestartGame);
+/**
+ * Bot moves
+ */
+
+function onCheckGameTie() {
+  if (emptySquares().length === 0) {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "#8bc34a";
+      cells[i].removeEventListener("click", onTurnClick, false);
+    }
+    onDeclareWinner("A Tie");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function emptySquares() {
+  return origBoard.filter((item) => typeof item === "number");
+}
+
+function botPicksSpot() {
+  return minimax(origBoard, AI_PLAYER).index;
+}
+
+function minimax(newBoard, player) {
+  let availableSpots = emptySquares();
+
+  if (onCheckWin(newBoard, HUMAN_PLAYER)) {
+    return { score: -10 };
+  } else if (onCheckWin(newBoard, AI_PLAYER)) {
+    return { score: 10 };
+  } else if (availableSpots.length === 0) {
+    return { score: 0 };
+  }
+
+  let moves = [];
+
+  for (let i = 0; i < availableSpots.length; i++) {
+    let move = {};
+    move.index = newBoard[availableSpots[i]];
+    newBoard[availableSpots[i]] = player;
+
+    if (player === AI_PLAYER) {
+      let result = minimax(newBoard, HUMAN_PLAYER);
+      move.score = result.score;
+    } else {
+      let result = minimax(newBoard, AI_PLAYER);
+      move.score = result.score;
+    } // end of if/else block
+
+    newBoard[availableSpots[i]] = move.index;
+    moves.push(move);
+  } // end of for look
+
+  let bestMove;
+
+  if (player === AI_PLAYER) {
+    let bestScore = -10000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    } // end of for loop
+  } else {
+    let bestScore = 10000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
+} // end of minimax func()
